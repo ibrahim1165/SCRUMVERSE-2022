fdf8f3e9caf164c934b96a5862e985266c457f65
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-02 19:51:38 +0600

    navbar added

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div
+        style={{ 
+            backgroundColor: '#9921E8',
+            backgroundImage:"cover",
+            width: '100%',
+            height: '100%',
+        }}
+        >
             <h2>This is a header</h2>
             
         </div>
