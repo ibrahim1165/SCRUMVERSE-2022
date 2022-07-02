534f8a9a6e6e58bb2089c83ccb6d67ccbdde31e8
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-02 23:41:31 +0600

    header pic added

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -1,19 +1,45 @@
 import React from 'react';
+import "./Header.css"
+import pic1 from '../../assest/Ellipse 1.png'
+import pic2 from '../../assest/Ellipse 2.png'
+import pic3 from '../../assest/Ellipse 3.png'
+import pic4 from '../../assest/Ellipse 4.png'
 
 const Header = () => {
+
     return (
         <div
-        style={{ 
-            backgroundColor: '#9921E8',
-            backgroundImage:"cover",
-            width: '100%',
-            height: '100%',
-        }}
-        >
-            <h2>This is a header</h2>
-            
+            style={{
+                backgroundColor: '#9921E8',
+                backgroundImage: "cover",
+            }} className="max-w-screen h-screen">
+                <div className="flex">
+
+                <div className="mt-14 px-8 justify-start">
+                    <p className="bg-white rounded-lg p-1">#MOST TRUSTED CERTIFICATIONS BY THE INDUSTRY</p>
+                    <div className="mt-8"> 
+                        <h2 className="text-white text-3xl">Unleash Your SCRUM Skills,<br />
+                        Grab More Attention To Résumé</h2>
+                    </div>
+                    <button className="btn btn-outline text-white mt-6">ENROLL NOW</button>
+
+                    <div className="time">
+
+                    </div>
+                </div>
+
+                <div className="">
+                   <img className="img1" src={pic4} alt="" />
+                   <img className="img2" src={pic3} alt="" />
+                    <img className="img3" src={pic2} alt="" />
+                    <img className="img4" src={pic1} alt="" />
+                </div>
+
+                </div>
+
         </div>
-    );
+
+    )
 };
 
 export default Header;
--- src/Pages/Navber/Navber.js
+++ src/Pages/Navber/Navber.js
@@ -5,7 +5,8 @@ const Navber = () => {
 
 
     return (
-        <div class="navbar bg-base-100">
+        <div class="navbar bg-base-100 text-white"
+        style={{backgroundColor: '#9921E8'}}>
             <div class="navbar-start">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -22,14 +23,15 @@ const Navber = () => {
                         <li><a>BLOGS</a></li>
                     </ul>
                 </div>
-                <div className="w-[36px] h-[26px] bg-black rounded-lg">
-                    <img src={bg} alt="" />
+                <div className=" rounded-lg">
+                    <img className="w-[30px] h-[26px] mx-4" src={bg} alt="" />
                 </div>
                 <a class="text-black">
-                    <h4 className="">SCRUMVERSE</h4>
+                    <h4 className="text-white">SCRUMVERSE</h4>
                 </a>
             </div>
-            <div class="navbar-center hidden lg:flex">
+            <div class="navbar-center hidden lg:flex"
+            >
                 <ul class="menu menu-horizontal p-0">
                     <li><a>HOME</a></li>
                     <li><a>ABOUT</a></li>
